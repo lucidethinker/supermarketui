a759e24bc7f29724ec2c6f451cc884b8ddedcb2d
lucidethinker <jasimihsan2@gmail.com>
2023-10-07 11:53:04 +0530

    Ui update

--- src/components/Staffs/OrdersList.jsx
+++ src/components/Staffs/OrdersList.jsx
@@ -5,6 +5,9 @@ import { GetAllUser } from "../../services/Users/api";
 const OrdersList = () => {
   const [oList, setoList] = useState([]);
   const [uList, setuList] = useState([]);
+  const [dateTotalMap, setDateTotalMap] = useState({});
+  const [showTotalTable, setShowTotalTable] = useState(false); // Track whether to show the total table
+
   useEffect(() => {
     async function fetchData() {
       let res = await GetAllOrders();
@@ -12,42 +15,32 @@ const OrdersList = () => {
       let u = await (await GetAllUser()).json();
       setuList(u);
       setoList(data);
+
+      // Calculate total amount by date
+      const totalByDate = {};
+      data.forEach((item) => {
+        const date = item.orderDate.split(" ")[0];
+        if (!totalByDate[date]) {
+          totalByDate[date] = 0;
+        }
+        totalByDate[date] += item.totalAmount;
+      });
+      setDateTotalMap(totalByDate);
     }
     fetchData();
   }, []);
+
+  const toggleTotalTable = () => {
+    setShowTotalTable(!showTotalTable);
+  };
+
   return (
     <div>
-      <label htmlFor="search" className="block font-bold">
-        Search
-      </label>
-      <select
-        name="search"
-        id="search"
-        className="w-1/2 px-4 py-2 border rounded-md"
-        onChange={async (e) => {
-          if (e.target.value != "") {
-            let a = await (await GetAllOrders()).json();
-            setoList([...a.filter((i) => i.userId == e.target.value)]);
-          }
-        }}
-      >
-        <option value="" selected disabled>
-          Select Category
-        </option>
-        {uList.map((user) => (
-          <option key={user.userId} value={user.userId}>
-            {user.fullName}
-          </option>
-        ))}
-      </select>
       <table className="min-w-full table-auto border border-collapse border-gray-200 mt-5">
         <thead>
           <tr>
             <th className="px-4 py-2 bg-blue-300 border border-gray-200 text-white">
               Sl.No
-            </th>
-            <th className="px-4 py-2 bg-blue-300 border border-gray-200 text-white">
-              Cashier
             </th>
             <th className="px-4 py-2 bg-blue-300 border border-gray-200 text-white">
               Time & Date
@@ -71,9 +64,6 @@ const OrdersList = () => {
             <tr key={index} className="hover:bg-gray-100">
               <td className="px-4 py-2 border border-gray-200">{index + 1}</td>
               <td className="px-4 py-2 border border-gray-200">
-                {uList.find((i) => i.userId == item.userId).fullName}
-              </td>
-              <td className="px-4 py-2 border border-gray-200">
                 {item.orderDate}
               </td>
               <td className="px-4 py-2 border border-gray-200">
@@ -92,7 +82,40 @@ const OrdersList = () => {
           ))}
         </tbody>
       </table>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-blue-500 text-white font-semibold py-2 px-4 my-4"
+          onClick={toggleTotalTable}
+        >
+          Show Total Amount by Date
+        </button>
+
+        {showTotalTable && (
+          <div className="mt-4">
+            <table className="w-full border border-collapse border-gray-300">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="px-4 py-2 text-left">Date</th>
+                  <th className="px-4 py-2 text-left">Total Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(dateTotalMap).map((date, index) => (
+                  <tr
+                    key={index}
+                    className={index % 2 === 0 ? "bg-gray-200" : ""}
+                  >
+                    <td className="px-4 py-2">{date}</td>
+                    <td className="px-4 py-2">{dateTotalMap[date]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
+
 export default OrdersList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [loginStatus, setLoginStatus] = useState(Cookies.get("loginStatus"));
+  const [loginUserType, setUserType] = useState(Cookies.get("loginUserType"));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoginStatus(Cookies.get("loginStatus"));
+    setUserType(Cookies.get("loginUserType"));
+  }, [Cookies.get("loginStatus"), Cookies.get("loginUserType")]);
   return (
     <div className="bg-blue-500 min-h-screen flex items-center justify-center">
-      <div className="text-white text-4xl font-bold">Welcome to My Supermarket</div>
+      <div className="text-white text-4xl font-bold">
+        {loginStatus == null && "Super Market Managment System "}
+        {loginUserType == 0 && " Welcome Admin"}
+        {loginUserType == 1 && " Welcome Staff "}
+      </div>
     </div>
   );
 };
